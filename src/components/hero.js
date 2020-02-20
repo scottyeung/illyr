8418a796665fdce44809f7c506d4fc306bb977c4
Scott Yeung <micold@gmail.com>
2020-02-20 14:39:50 +0000

    Video BG

--- src/components/hero.js
+++ src/components/hero.js
@@ -15,25 +15,26 @@ const videoJsOptions = {
 
 export default ({ data }) => (
   <div className={styles.hero}>
-  <ReactPlayer url='https://rebecca.inverted-audio.com/videos/encrypt/Brave Echo/playlist.m3u8' 
-              config={{
-              file: {
-                forceHLS: true,
-                hlsOptions: {
-                  xhrSetup: function(xhr, url) {
-                    xhr.withCredentials = true
-                    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-                  },
-                }
-              }
-            }}
+  <ReactPlayer
+    width="100%"
+    height="100vh" 
+    muted playing
+    crossOrigin="anonymous"
+    url='https://rebecca.inverted-audio.com/videos/encrypt/Brave Echo/playlist.m3u8' 
+    config={{
+    file: {
+      forceHLS: true,
+      hlsOptions: {
+      }
+    }
+  }}
     />
     {/* <track kind="captions" srcLang="en" src={Captions} /> */}
-    <Img className={styles.heroImage} alt={data.name} fluid={data.heroImage.fluid} />
-    <div className={styles.heroDetails}>
+    {/* <Img className={styles.heroImage} alt={data.name} fluid={data.heroImage.fluid} /> */}
+    {/* <div className={styles.heroDetails}>
       <h3 className={styles.heroHeadline}>{data.name}</h3>
       <p className={styles.heroTitle}>{data.title}</p>
       <p>{data.shortBio.shortBio}</p>
-    </div>
+    </div> */}
   </div>
 )
