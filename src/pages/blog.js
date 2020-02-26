5f4f5f1fdf352f7c155e77d40ffd2290f1dd61f4
Scott Yeung <micold@gmail.com>
2020-02-26 00:49:21 +0000

    Data attr on ReactPlayer

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -52,11 +52,6 @@ export const pageQuery = graphql`
           slug
           publishDate(formatString: "MMMM Do, YYYY")
           tags
-          heroImage {
-            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
-              ...GatsbyContentfulFluid_tracedSVG
-            }
-          }
           description {
             childMarkdownRemark {
               html
