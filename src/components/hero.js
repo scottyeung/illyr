77564485bd92bdcc1ec7d979243ce3eefb5f49b9
Scott Yeung <micold@gmail.com>
2020-02-26 00:36:31 +0000

    Data attr on ReactPlayer

--- src/components/hero.js
+++ src/components/hero.js
@@ -7,28 +7,25 @@ const isIos = () => {
   return /iphone|ipad|ipod/.test(userAgent)
 }
 
-export default ({ data, isPlaying, muted }) => {
-
-  let video
-
-  return (
-    <div className={styles.hero}>
-      <ReactPlayer
-        ref={el => video = el}
-        onReady={() => {video.getInternalPlayer().setAttribute('data-keepplaying', '')}}
-        width="100%"
-        height="100vh"
-        muted={muted} playing={isPlaying} loop
-        crossOrigin="anonymous"
-        url={data.video} 
-        config={{
-          file: {
-            forceHLS: !isIos,
-            forceVideo: true,
+export default ({ data, isPlaying, muted }) => (
+  <div className={styles.hero}>
+    <ReactPlayer
+      width="100%"
+      height="100vh"
+      muted={muted} playing={isPlaying} loop
+      crossOrigin="anonymous"
+      url={data.video} 
+      config={{
+        file: {
+          forceHLS: !isIos,
+          forceVideo: true,
+          attributes: {
+            'data-keepplaying': ''
           }
-        }}
-      />
-    </div>
-  )
-}
+        }
+      }}
+    />
+  </div>
+)
+
 
