eeaf9570dc1bb50906f733da33c47e2ebf4ef92c
Scott Yeung <micold@gmail.com>
2020-03-12 11:10:52 +0100

    Autoplay Issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -122,7 +122,7 @@ class RootIndex extends React.Component {
                         width="100%"
                         height="100vh"
                         autoPlay
-                        url={ index == this.state.currentIndex && !this.state.isBTS ? node.video : ''} 
+                        url={ node.video } 
                         playing={ index == this.state.currentIndex && !this.state.isBTS ? true : false }
                         muted={ isChrome ? true : this.state.muted }
                         loop
